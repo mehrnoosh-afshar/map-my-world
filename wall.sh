5f6a4eab85532924bbe251bd213b93fd732d0390
mehrnoosh-afshar <56322879+mehrnoosh-afshar@users.noreply.github.com>
2022-10-18 12:54:04 -0600

    Update wall.sh

--- wall.sh
+++ wall.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 export TURTLEBOT_3D_SENSOR=kinect
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e "roslaunch my_robot world.launch" &
 sleep 5
-xterm -e "roslaunch custom_gmapping_demo.launch" &
+xterm -e "roslaunch my_robot mapping.launch" &
 sleep 5
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xtrem -e "rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
 sleep 5
 xterm -e "rosrun wall_follower wall_follower" &
